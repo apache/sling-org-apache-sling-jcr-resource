ba4964bf9abf276b43a816962bdf6cc184c731aa
Konrad Windszus <kwin@apache.org>
2021-11-03 21:15:27 +0100

    add tests

--- src/test/java/org/apache/sling/jcr/resource/internal/helper/jcr/JcrResourceProviderTest.java
+++ src/test/java/org/apache/sling/jcr/resource/internal/helper/jcr/JcrResourceProviderTest.java
@@ -20,10 +20,17 @@ package org.apache.sling.jcr.resource.internal.helper.jcr;
 
 import java.security.Principal;
 
+import javax.jcr.Node;
 import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.nodetype.NodeType;
 
+import org.apache.jackrabbit.commons.JcrUtils;
+import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.spi.resource.provider.ResolveContext;
+import org.apache.sling.spi.resource.provider.ResourceContext;
 import org.junit.Assert;
 import org.mockito.Mockito;
 import org.osgi.framework.ServiceReference;
@@ -56,6 +63,42 @@ public class JcrResourceProviderTest extends SlingRepositoryTestBase {
         ResolveContext ctx = Mockito.mock(ResolveContext.class);
         Mockito.when(ctx.getProviderState()).thenReturn(new JcrProviderState(session, null, false));
         Assert.assertNotNull(jcrResourceProvider.adaptTo(ctx, Principal.class));
+    }
+
+    public void testOrderBefore() throws RepositoryException, PersistenceException {
+        Node parentNode = session.getRootNode().addNode("parent", NodeType.NT_UNSTRUCTURED);
+        parentNode.addNode("child1", NodeType.NT_UNSTRUCTURED);
+        parentNode.addNode("child2", NodeType.NT_UNSTRUCTURED);
+        parentNode.addNode("child3", NodeType.NT_UNSTRUCTURED);
+        session.save();
+
+        ResolveContext ctx = Mockito.mock(ResolveContext.class);
+        JcrProviderState state = new JcrProviderState(session, null, false);
+        Mockito.when(ctx.getProviderState()).thenReturn(state);
+        Resource parent = jcrResourceProvider.getResource(ctx, "/parent", ResourceContext.EMPTY_CONTEXT, null);
+        Assert.assertNotNull(parent);
+        // order with invalid names
+        try {
+            jcrResourceProvider.orderBefore(ctx, parent, "child3", "child4");
+        } catch (PersistenceException e) {
+            // expected
+        }
+        // order successfully
+        Assert.assertTrue(jcrResourceProvider.orderBefore(ctx, parent, "child2", "child1"));
+        
+        // order already established
+        Assert.assertFalse(jcrResourceProvider.orderBefore(ctx, parent, "child2", "child1"));
+        
+        // order child2 at end
+        Assert.assertTrue(jcrResourceProvider.orderBefore(ctx, parent, "child2", null));
+        
+        // order child2 at end again
+        Assert.assertFalse(jcrResourceProvider.orderBefore(ctx, parent, "child2", null));
+        
+        // make sure nothing is persisted until save
+        jcrResourceProvider.revert(ctx);
+        
+        Assert.assertTrue(jcrResourceProvider.orderBefore(ctx, parent, "child2", null));
     }
 }
 
